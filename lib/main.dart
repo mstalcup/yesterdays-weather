e77b2e11e0de5034b556c3de69e10da09308ce8f
Michael <mastalcup@hotmail.com>
2019-05-19 16:34:09 -0500

    prevent screen rotation

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:intl/intl.dart';
 import 'package:location/location.dart';
 import 'package:flutter/rendering.dart';
 import 'package:geocoder/geocoder.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +14,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Weather App',
       theme: ThemeData(
